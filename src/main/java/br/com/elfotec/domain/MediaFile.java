fff022cccb185e6fd8225200ac1ca6c0045145f4
Pedro Lucas Garcia <190115548@aluno.unb.br>
2024-08-22 10:20:44 -0300

    Transforma exclusão física de media file em exclusão lógica

--- src/main/java/br/com/elfotec/domain/MediaFile.java
+++ src/main/java/br/com/elfotec/domain/MediaFile.java
@@ -2,10 +2,19 @@ package br.com.elfotec.domain;
 
 import java.io.Serializable;
 import java.time.Instant;
-import javax.persistence.*;
-import javax.validation.constraints.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 /**
  * A MediaFile.
@@ -13,6 +22,8 @@ import org.hibernate.annotations.CacheConcurrencyStrategy;
 @Entity
 @Table(name = "media_file")
 @Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+@SQLDelete(sql = "UPDATE media_file SET data_exclusao = CURRENT_TIMESTAMP WHERE id = ?")
+@Where(clause = "data_exclusao IS NULL")
 public class MediaFile implements Serializable {
 
     private static final long serialVersionUID = 1L;
